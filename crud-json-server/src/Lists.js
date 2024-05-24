99e34a158c1b967aa9c81c7779c442f36aac4a6e
Crystal P <pittmanc82@gmail.com>
2024-05-23 17:16:50 -0700

    unset change

--- crud-json-server/src/Lists.js
+++ crud-json-server/src/Lists.js
@@ -2,30 +2,30 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Lists(props) {
-    let listrows = [];
-    props.alldata.forEach(element => {
-      listrows.push(
-        <tr key={element.id}>
-            <td>{element.id}</td>
-            <td>{element.title}</td>
-            <td>{element.author}</td>
+  let listrows = [];
+  props.alldata.forEach((element) => {
+    listrows.push(
+      <tr key={element.id}>
+        <td>{element.id}</td>
+        <td>{element.title}</td>
+        <td>{element.author}</td>
+      </tr>
+    );
+  });
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Update</th>
+          <th>Delete</th>
         </tr>
-      )
-    })
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Update</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>{listrows}</tbody>
-        </table>
-    )
+      </thead>
+      <tbody>{listrows}</tbody>
+    </table>
+  );
 }
 
 export default Lists;
